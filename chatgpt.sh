afaeee78b5299d68bb735251f28b89ec86c11b65
Stefan Teunissen <stefanteunissen1@gmail.com>
2022-12-13 21:15:09 +0100

    Init

---
+++ chatgpt.sh
@@ -0,0 +1,13 @@
+# shellcheck disable=SC2046
+export $(grep -v '^#' .env | xargs)
+
+curl https://api.openai.com/v1/completions \
+  -s \
+  -H 'Content-Type: application/json' \
+  -H "Authorization: Bearer ${OPENAI_API_KEY}" \
+  -d '{
+  "model": "'"$MODEL"'",
+  "prompt": "'"$1"'",
+  "max_tokens": '"$MAX_TOKENS"',
+  "temperature": 0.3
+}' | jq -r ".choices[0].text"
